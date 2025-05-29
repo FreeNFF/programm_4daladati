0f6b53ba967d3c6cf25eb146f4a8faa18b124654
FreeNFF <nauisfricsons30@gmail.com>
2025-05-29 13:45:30 +0300

    Update agents.py

--- agents.py
+++ agents.py
@@ -11,16 +11,22 @@ print("\nAtlasītie dati:\n")
 
 with open("agenti.csv", mode="r", encoding="utf-8") as file:
     csv_reader = csv.DictReader(file)
-    
     for row in csv_reader:
         if row["TIPS"] == "Izglītības iestāde":
             print(row)
 
 with open("agenti.csv", mode="r", encoding="utf-8") as file:
     csv_reader = csv.DictReader(file)
-    print("Atlasītie dati:")
     for row in csv_reader:
         if row["TIPS"] == "Valsts iestāde":
             print(row)
 
+
+print("\nRīgas iestādes:\n")
+
+with open("agenti.csv", mode="r", encoding="utf-8") as file:
+    csv_reader = csv.DictReader(file)
+    for row in csv_reader:
+        if row["ADRESE"] == "Rīga":
+            print(row)
     
