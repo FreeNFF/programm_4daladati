1d87d324ee4adc3d929c20104884e750f9386198
FreeNFF <nauisfricsons30@gmail.com>
2025-05-29 12:37:04 +0300

    Update valstis.py

--- valstis.py
+++ valstis.py
@@ -1,3 +1,4 @@
+from audioop import lin2alaw
 import requests
 
 url = "https://restcountries.com/v3.1/all"
@@ -10,12 +11,21 @@ if response.status_code == 200:
 
 #Iegūst un izvada datus vispārpieņemtos nosaukumus
 
-print("Vispārpieņemtie valsts nosaukumi:")
+    print("Vispārpieņemtie valsts nosaukumi:")
 
-for valstis in dati:
-    nosaukums = valstis.get("name",{}).get("common","Nezināms")
-    print(nosaukums)
+    for valstis in dati:
+        nosaukums = valstis.get("name",{}).get("common","Nezināms")
+        print(nosaukums)
 
-kop_skaits = len(dati)
-print(f"Valsts kopējais skaits ir {kop_skaits}.")
+    kop_skaits = len(dati)
+    print(f"Valsts kopējais skaits ir {kop_skaits}.")
 
+    kop_populacija = sum(valstis["population"] for valstis in dati)
+    videja_populacija = kop_populacija / kop_skaits
+    print(f"Vidējais iedzīvotāju skaits ir {videja_populacija:.0f}")
+
+    lielaka_populacija = valstis["population"] = max
+    valsts_lielaka = lielaka_populacija.get("name",{}).get("common","Nezināms")
+    print(f"Lielākais iedzīvotāju skaits ir valstī {valsts_lielaka}.")
+
+    
