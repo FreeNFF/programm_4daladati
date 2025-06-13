33fa4905d7307a773d8ad0ef6e0f242e36d75883
FreeNFF <nauisfricsons30@gmail.com>
2025-06-13 10:03:58 +0300

    Update citasvalstis.py

--- citasvalstis.py
+++ citasvalstis.py
@@ -1,6 +1,15 @@
 import requests
 
-url = "https://restcountries.com/v2/name/{name}/fullText=true"
+valstsis = ["Latvia", "Estonia", "Lithuania", "Poland", "Germany", "France", "Spain", "Italy", "Portugal", "Greece"]
 
-response = requests.get(url)
-
+def dati_valsts():
+  url = f"https://restcountries.com/v2/name/{name}/fullText=true"
+  response = requests.get(url)
+  dati = response.json()[0]
+  return {
+    "Valsts" : dati["name"],
+    "Galvaspilsēta" : dati["capital"],
+    "Iedzīvotāji" : dati["population"],
+    "Valūta" : dati["currencies"][0]["name"],
+    "Reģions": dati["region"]
+    }
